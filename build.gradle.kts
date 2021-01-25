02822836128125ce7a6614b4727bbde3e6810f71
Or Noyman <or.noyman@gmail.com>
2021-01-25 02:24:28 +0200

    allow gradual conversion of submodules to MPP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-//plugins {
-//    id("com.github.ben-manes.versions").version(Versions.versions_plugin)
-//}
-
 buildscript {
     repositories {
         mavenLocal()
@@ -10,7 +6,6 @@ buildscript {
     }
 
     dependencies {
-        //classpath("com.github.ben-manes:gradle-versions-plugin:${Versions.versions_plugin}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
     }
 }
@@ -18,31 +13,37 @@ buildscript {
 
 
 subprojects {
-    group = "org.komputing"
+    // a trick to allow gradual conversion of modules to MPP
+    afterEvaluate {
+        if (!plugins.hasPlugin("mpp-module")) {
+            group = "org.komputing"
 
-    plugins.apply("kotlin")
-    plugins.apply("maven")
+            plugins.apply("kotlin")
+            plugins.apply("maven")
 
-    dependencies {
-        "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
+            dependencies {
+                "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
 
-        "testCompile"("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
-        "testCompile"("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
-        "testRuntime"("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
+                "testCompile"("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
+                "testCompile"("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
+                "testRuntime"("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
 
-        "testImplementation"("org.jetbrains.kotlin:kotlin-test")
+                "testImplementation"("org.jetbrains.kotlin:kotlin-test")
 
-        "testImplementation"("com.github.komputing:khex:1.0.0")
-    }
+                "testImplementation"("com.github.komputing:khex:1.0.0")
+            }
 
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        testLogging {
-            events("passed", "skipped", "failed")
+            tasks.withType<Test> {
+                useJUnitPlatform()
+                testLogging {
+                    events("passed", "skipped", "failed")
+                }
+            }
         }
     }
 
     repositories {
+        mavenLocal() // TODO: remove usage of maven local for KHex
         jcenter()
         maven("https://jitpack.io")
         maven("https://kotlin.bintray.com/kotlinx")
