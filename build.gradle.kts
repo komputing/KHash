949e27c6298cd1ca5f04d40aaaa29b4e3423dadc
Or Noyman <or.noyman@gmail.com>
2021-02-05 22:22:06 +0200

    remove non-multiplatform subprojects config

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,35 +13,6 @@ buildscript {
 
 
 subprojects {
-    // a trick to allow gradual conversion of modules to MPP
-    afterEvaluate {
-        if (!plugins.hasPlugin("mpp-module")) {
-            group = "org.komputing"
-
-            plugins.apply("kotlin")
-            plugins.apply("maven")
-
-            dependencies {
-                "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
-
-                "testCompile"("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
-                "testCompile"("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
-                "testRuntime"("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
-
-                "testImplementation"("org.jetbrains.kotlin:kotlin-test")
-
-                "testImplementation"("com.github.komputing:khex:1.0.0")
-            }
-
-            tasks.withType<Test> {
-                useJUnitPlatform()
-                testLogging {
-                    events("passed", "skipped", "failed")
-                }
-            }
-        }
-    }
-
     repositories {
         mavenLocal() // TODO: remove usage of maven local for KHex
         jcenter()
