80c9f72930fadd79e8ffd1c7d24629cfb25f4030
Or Noyman <or.noyman@gmail.com>
2021-02-05 21:00:44 +0200

    update bignum v0.2.4-SNAPSHOT -> v0.2.4 (stable)

--- keccak/build.gradle.kts
+++ keccak/build.gradle.kts
@@ -2,18 +2,11 @@ plugins {
     id("mpp-module")
 }
 
-repositories {
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
-}
-
 kotlin {
     sourceSets{
         commonMain {
             dependencies {
-                // TODO: switch to next stable version that will support the new JS IR compiler and include bug fixes to bitwise operations
-                implementation("com.ionspin.kotlin:bignum:0.2.4-SNAPSHOT")
+                implementation("com.ionspin.kotlin:bignum:0.2.4")
             }
         }
     }
