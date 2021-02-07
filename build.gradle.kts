c66af0cf0208b1b09c93a4d13f45c96265788802
Or Noyman <or.noyman@gmail.com>
2021-02-08 01:15:06 +0200

    add aggregate test report task to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,20 @@
+plugins {
+    base
+}
 
+val testAggregateReport = tasks.register<TestReport>("testAggregateReport") {
+    group = "Reporting"
+    description = "Collect aggregate test reports of all sub-modules."
+    destinationDir = file("$buildDir/reports/tests")
+    reportOn(subprojects.map {
+        it.tasks.withType<AbstractTestTask>()
+    })
+}
+
+subprojects {
+    afterEvaluate {
+        tasks.withType<AbstractTestTask> {
+            finalizedBy(testAggregateReport)
+        }
+    }
+}
