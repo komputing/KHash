285a510523f9e5bf702c4a7e98616bf4d53e6278
Or Noyman <or.noyman@gmail.com>
2021-02-08 01:04:57 +0200

    add comment for the GitHub Packages public access workaround

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ dependencyResolutionManagement {
             url = uri("https://maven.pkg.github.com/fullkomnun/KHex")
             credentials {
                 username = "token"
+                // see: https://github.community/t/download-from-github-package-registry-without-authentication/14407/44
                 password = "\u0035\u0035\u0039\u0032\u0061\u0030\u0061\u0038\u0033\u0065\u0032\u0030\u0033\u0035\u0038\u0061\u0034\u0066\u0031\u0032\u0063\u0039\u0031\u0061\u0033\u0065\u0030\u0030\u0066\u0038\u0030\u0031\u0039\u0064\u0035\u0036\u0039\u0032\u0037\u0066"
             }
         }
